290efafb5e83dafe00da6ca4c6a8e332aaf4c73d
unknown <mominalishan@gmail.com.com>
2022-06-18 15:52:24 +0500

    ui design and functionality

--- src/component/questionsScreen.js
+++ src/component/questionsScreen.js
@@ -4,9 +4,11 @@ const QuestionsScreen = () => {
 
     const tempArray =[]
     const [cleanData, setCleanData] = useState([])
-    const [correctAns, setCorrectAns] = useState("")
+    const [correctAns, setCorrectAns] = useState(false)
+    const [clickYes, setClickYes] = useState(false)
     const [clickIndex, setClickIndex] = useState(0)
-
+    const [btnIndex, setBtnIndex] = useState(-1)
+   /*  console.log("co", correctAns) */
     const getAllQuestions = () => {
         questions.forEach((item, index) => {
             const tempIncorrect =[]
@@ -27,36 +29,53 @@ const QuestionsScreen = () => {
             setCleanData(tempArray)
         })
     }
-    const handleAns = (index) => {
+    const handleAns = (index, elem) => {
         const ans = cleanData[index].correct_answer
-        setCorrectAns(ans)
+    console.log("ans", ans)
+    console.log("elem", elem)
+        
+        if(elem === ans) {
+            setCorrectAns(true)
+        } else {
+            setCorrectAns(false)
+        }
     }
     useEffect(() => {
         getAllQuestions()
     }, [])
     
-    console.log("cleanData", cleanData)
-  return (
+/*     console.log("cleanData", cleanData)
+ */  return (
     <div className='main'>
         <p className='heading'>The Quiz Game</p>
         <div className='box'>
                 <p className='question'>{cleanData[clickIndex]?.question}</p>
                 <div className='options'>
-
+                {/* sort(() => Math.random() - 0.5) */}
                 {
-                    cleanData[clickIndex]?.options.sort(() => Math.random() - 0.5).map((elem, index) => (
-                        <div onClick={() => { handleAns(clickIndex)}}>
+                    cleanData[clickIndex]?.options.map((elem, index) => (
+/*                         <div onClick={() => { handleAns(clickIndex)}}>
                             {
                             correctAns === elem ? <button className='btn'>{elem}</button> : <button className='btn'>{elem}</button>
                         }                        
-                    </div>
+                    </div> */
+                        <div>
+                            <button
+                                 onClick={() => {handleAns(clickIndex, elem); setBtnIndex(index); setClickYes(true)}} 
+                                 className={btnIndex === index ? correctAns ? 'btn-correct' : "btn-wrong" : "btn"}
+                                 disabled ={clickYes}
+                                 >
+                                    {elem}
+
+                            </button> 
+                        </div>
                     ))
                 }
                 </div>
                 <div className='bottom'>
-                    <i className="fas fa-caret-left" onClick={() => setClickIndex(clickIndex - 1)}></i>
+                    <i className="fas fa-caret-left" onClick={() => {setClickIndex(clickIndex - 1); setBtnIndex(-1); setClickYes(false)}}></i>
                     <p className='number'><span>{clickIndex+1}</span>/20</p>
-                    <i className="fas fa-caret-right" onClick={() => setClickIndex(clickIndex+1)}></i>
+                    <i className="fas fa-caret-right" onClick={() => {setClickIndex(clickIndex+1); setBtnIndex(-1); setClickYes(false)}}></i>
                 </div>
                 
         </div>
